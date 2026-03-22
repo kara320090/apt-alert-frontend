ba6665b8d63483bda74602d3cb7cd5d31d4d9875
kara320090 <bongbong9292@naver.com>
2026-03-22 21:04:41 +0900

    dd

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: [
+              "default-src 'self'",
+              "script-src 'self' 'unsafe-eval' 'unsafe-inline' https://dapi.kakao.com",
+              "style-src 'self' 'unsafe-inline' https://dapi.kakao.com",
+              "img-src 'self' data: blob: https://*.kakao.com https://*.kakaocdn.net",
+              "connect-src 'self' https://*.kakao.com https://*.kakaocdn.net",
+              "frame-src 'self' https://*.kakao.com",
+            ].join("; "),
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
